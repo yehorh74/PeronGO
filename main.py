84ac8d02be6fb1b3dd778306f330d69d4ead887f
yehorh74 <danedred15@gmail.com>
2026-07-27 11:44:47 +0200

    Initial commit - aplikacja Peron w Flet

---
+++ main.py
@@ -0,0 +1,47 @@
+import os
+from urllib import response
+import flet as ft
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_KEY = os.getenv("X-API-Key")
+API_URL = os.getenv("PLK_API_URL", "https://pdp-api.plk-sa.pl/api/v1")
+
+def main(page: ft.Page):
+    page.title = "PeronGO – Tablica odjazdów"
+    page.theme_mode = ft.ThemeMode.DARK
+    page.padding = 20
+
+    status_label = ft.Text(value="Kliknij przycisk, aby pobrać dane", size=16)
+
+    def fetch_data(e):
+        if not API_KEY:
+            status_label.value = "Błąd: Brak klucza X-API-Key w pliku .env!"
+            page.update()
+            return
+
+        headers = {
+            "X-Api-Key": API_KEY,
+            "Accept": "application/json"
+        }
+
+        try:
+            response = requests.get(f"{API_URL}/stations", headers=headers, timeout=5)
+            if response.status_code == 200:
+                status_label.value = "Połączono z API PKP PLK pomyślnie!"
+            else:
+                status_label.value = f"Błąd API: {response.status_code}"
+        except Exception as err:
+            status_label.value = f"Błąd połączenia: {err}"
+        
+        page.update()
+
+    page.add(
+        ft.Text("PeronGO", size=28, weight=ft.FontWeight.BOLD),
+        ft.ElevatedButton("Sprawdź połączenie z API", on_click=fetch_data),
+        status_label
+    )
+
+ft.app(target=main)
